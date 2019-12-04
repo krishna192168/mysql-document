938d0dfa124708140906eebdda5537eb49ea9ecf
krishnamoorthy <58415893+krishna192168@users.noreply.github.com>
2019-12-04 11:25:33 +0530

    Naming convention updated for tables

--- PL_SQL/tnstc_v2.sql
+++ PL_SQL/tnstc_v2.sql
@@ -23,11 +23,11 @@ BEGIN
 	END;
 	BEGIN
     SAVEPOINT bookticket;
-		SELECT IFNULL(SUM(bookTicket.number_of_seat),0) INTO bookedSeats FROM book_ticket bookTicket WHERE bookTicket.bus_id = bus_id;
-        SELECT number_of_seats INTO busSeats FROM tnstc_bus WHERE id = bus_id;
+		SELECT IFNULL(SUM(bookTicket.number_of_seat),0) INTO bookedSeats FROM book_tickets bookTicket WHERE bookTicket.bus_id = bus_id;
+        SELECT number_of_seats INTO busSeats FROM buses WHERE id = bus_id;
         SET availableSeats = busSeats - bookedSeats;
         IF availableSeats >= number_of_bus_seats THEN
-			INSERT INTO book_ticket(
+			INSERT INTO book_tickets(
 				user_id,
 				bus_id,
 				pickup_point_id,
@@ -52,7 +52,40 @@ BEGIN
     END;
 END||
 DELIMITER ;
-CALL tnstc_ticket_booking(1,1,1,2,5,500);
+CALL tnstc_ticket_booking(1,1,1,2,5,5000);
 
-SELECT * FROM book_ticket;
+SELECT * FROM book_tickets;
+SELECT * FROM users;
 SELECT * FROM user_wallet;
+INSERT INTO users(
+	name,
+    email,
+    password
+) VALUES(
+	'krishna',
+    'krishna@gmail.com',
+    'pass123'
+);
+INSERT INTO user_wallet(user_id,amount)VALUES(
+	1,
+    1500.05
+);
+INSERT INTO bus_categories(
+	category	
+)VALUES(
+	'ULTRA DELUX'
+);
+INSERT INTO buses(
+	bus_number,
+    bus_category_id,
+    number_of_seats
+) VALUES(
+	'TN1009',
+    1,
+    80
+);
+INSERT INTO cities(
+	city
+)VALUES(
+	'madurai'
+);
