21923301f0cf74ff9833543fcb8af77aebed3bf4
krishnamoorthy <58415893+krishna192168@users.noreply.github.com>
2019-12-04 11:27:10 +0530

    Naming conventions updated for tables

--- PL_SQL/tnstc_v1.sql
+++ PL_SQL/tnstc_v1.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA tnstc_db;
 USE tnstc_db;
-CREATE TABLE tnstc_user(
+CREATE TABLE users(
 	id int AUTO_INCREMENT,
 	name varchar(50) NOT NULL,
 	email varchar(50) NOT NULL,
@@ -10,30 +10,30 @@ CREATE TABLE tnstc_user(
 	CONSTRAINT pk_user_id PRIMARY KEY(id),
     CONSTRAINT uk_email UNIQUE(email)
 );
-CREATE TABLE tnstc_bus_type(
+CREATE TABLE bus_categories(
 	id int AUTO_INCREMENT,
 	category varchar(50) NOT NULL,
 	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
 	updated_on timestamp,
-	PRIMARY KEY(id)
+	CONSTRAINT pk_bus_category_id PRIMARY KEY(id)
 );
-CREATE TABLE tnstc_bus(
+CREATE TABLE buses(
 	id int AUTO_INCREMENT,
 	bus_number varchar(20),
-	bus_type_id int NOT NULL,
+	bus_category_id int NOT NULL,
 	created_on timestamp DEFAULT CURRENT_TIMESTAMP,
 	updated_on timestamp,
-	PRIMARY KEY(id),
-	FOREIGN KEY(bus_type_id) REFERENCES tnstc_bus_type(id)
+	CONSTRAINT pk_buses_id PRIMARY KEY(id),
+	CONSTRAINT fk_bus_category_id FOREIGN KEY(bus_category_id) REFERENCES bus_categories(id)
 );
-ALTER TABLE tnstc_bus ADD number_of_seats int NOT NULL;
-DESC tnstc_bus;
-CREATE TABLE tnstc_city(
+ALTER TABLE buses ADD number_of_seats int NOT NULL;
+DESC buses;
+CREATE TABLE cities(
 	id int AUTO_INCREMENT,
 	city varchar(50),
-	PRIMARY KEY(id)
+	CONSTRAINT pk_cities_id PRIMARY KEY(id)
 );
-CREATE TABLE book_ticket(
+CREATE TABLE book_tickets(
 	id int AUTO_INCREMENT,
 	user_id int,
 	bus_id int NOT NULL,
@@ -42,10 +42,10 @@ CREATE TABLE book_ticket(
 	amount double(15,2) NOT NULL,
 	number_of_seat int NOT NULL, 
 	CONSTRAINT pk_bookticket_id PRIMARY KEY(id),
-	CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES tnstc_user(id),
-	CONSTRAINT fk_pickup_point_id FOREIGN KEY(pickup_point_id) REFERENCES tnstc_city(id),
-	CONSTRAINT fk_drop_point_id FOREIGN KEY(drop_point_id) REFERENCES tnstc_city(id),
-	CONSTRAINT fk_bus_id FOREIGN KEY(bus_id) REFERENCES tnstc_bus(id),
+	CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(id),
+	CONSTRAINT fk_pickup_point_id FOREIGN KEY(pickup_point_id) REFERENCES cities(id),
+	CONSTRAINT fk_drop_point_id FOREIGN KEY(drop_point_id) REFERENCES cities(id),
+	CONSTRAINT fk_bus_id FOREIGN KEY(bus_id) REFERENCES buses(id),
     CONSTRAINT ck_number_of_seat CHECK(number_of_seat >= 0)
 );
 CREATE TABLE user_wallet(
@@ -53,6 +53,6 @@ CREATE TABLE user_wallet(
     user_id int NOT NULL,
 	amount double(15,2) NOT NULL,
 	CONSTRAINT pk_user_wallet_id PRIMARY KEY(id),
-    CONSTRAINT fk_wallet_user_id FOREIGN KEY(user_id) REFERENCES tnstc_user(id),
+    CONSTRAINT fk_userwallet_id FOREIGN KEY(user_id) REFERENCES users(id),
 	CONSTRAINT ck_amount CHECK(amount > 0)
 );
